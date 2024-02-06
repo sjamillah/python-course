c572e9aea7aca26f46882fcc99dc44894bf3f0ad
sjamillah <j.ssozi@alustudent.com>
2024-02-06 17:58:38 +0200

    randomization

--- 100-days-projects/tail_head.py
+++ 100-days-projects/tail_head.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #script to pick by random tails or heads
 import random
-message = print("What is your choice? 0 or 1?")
+message = input("What is your choice? 0 or 1?")
 choice = random.randint(0, 1)
 if choice == 0:
     print("Tails")

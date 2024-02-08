f8be262237b0b0174a9911dac2a6ea8b1b5d084c
sjamillah <j.ssozi@alustudent.com>
2024-02-08 23:46:26 +0200

    changes in the game

--- 100-days-projects/game.py
+++ 100-days-projects/game.py
@@ -30,7 +30,9 @@ images = [Rock, Paper, Scissors]
 print(images[choice]) #prints the images based on your choice btn 0,1,2
 computer_chose = random.randint(0, 2) #computer chooses a random number in the range
 print(f"Computer chose: {images[computer_chose]}") #prints the image based on the computer choice
-if computer_chose < choice:
+if ((computer_chose == 2 and choice == 0) or
+    (computer_chose == 1 and choice == 2) or
+    (computer_chose == 0 and choice == 1)):
   print("You win")
 elif computer_chose > choice:
   print("You lose")

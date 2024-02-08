8f3b6e78d93797e6965920244c497b9092cd4970
sjamillah <j.ssozi@alustudent.com>
2024-02-09 01:04:28 +0200

    adding even numbers

---
+++ 100-days-projects/even_number_sum.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#Script to add even numbers in a set range
+
+print("Adds all the even numbers from 0 to 1000")
+target = int(input("Please input numbers from 0 to 1000: "))
+total = 0
+for num in range(1, target):
+  if num % 2 == 0:
+    total += num
+    total_num = total + target
+print(f"The total is: {total_num}")
+
+#usr/bin/python3
+#second option
+print("Adds all the even numbers from 0 to 1000")
+target = int(input("Please input numbers from 0 to 1000: "))
+total = 0
+for num in range(2, target + 1, 2):
+    total += num
+print(f"The total is: {total}")

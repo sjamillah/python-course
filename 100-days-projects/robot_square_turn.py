cebaca211b1ed0a23285749e925ff86fbbc919e2
sjamillah <j.ssozi@alustudent.com>
2024-02-12 17:06:45 +0200

    robot square turning

---
+++ 100-days-projects/robot_square_turn.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#Script to turn a robot in a square
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()

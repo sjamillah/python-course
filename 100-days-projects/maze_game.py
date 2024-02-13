3c0aa43df8f76f0868461f35250c3dee4d87fd29
sjamillah <j.ssozi@alustudent.com>
2024-02-13 16:59:25 +0200

    maze world

---
+++ 100-days-projects/maze_game.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#Script to make a maze game
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
+while at_goal() != True:
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
